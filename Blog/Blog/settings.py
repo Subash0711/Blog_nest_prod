e6139fe38ee22e64ed809f6ddf222323c87b8614
Subash0711 <109809088+Subash0711@users.noreply.github.com>
2024-01-29 12:18:24 +0530

    add changes in Space objcets

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -157,9 +157,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 AWS_LOCATION = 'blognest'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = []
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
